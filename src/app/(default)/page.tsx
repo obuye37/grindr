f88c38cbe36ed2281ffda76e762c0c98eaf0da63
orison-j <obuye37@gmail.com>
2025-03-05 05:42:34 +0100

    some edits for vercel

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, MouseEvent } from "react";  
+import React, { useState } from "react";  
 // import useWindowSize from '@rooks/use-window-size'
 
 import JoinTG from "@/components/JoinTG";
